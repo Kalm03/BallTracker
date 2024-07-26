0514466830068e641f2c599003ebc74c75a5767d
Kalm03 <kamranalam.ra@gmail.com>
2024-07-26 16:17:43 -0700

    restructured

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 let ball = {
     position: { i: 0, j: 0, k: 0 },
@@ -53,49 +53,6 @@ app.post('/reset', (req, res) => {
 
 app.get('/status', (req, res) => {
     res.json(ball);
-});
-
-// Add a new card
-app.post('/cards', (req, res) => {
-    const { name, initiative } = req.body;
-    if (name && !isNaN(initiative)) {
-        let id = stringify(cards.length + 1);
-        cards.push({ id, name, initiative });
-        cards.sort((a, b) => b.initiative - a.initiative);
-        res.status(201).send(cards);
-    } else {
-        res.status(400).send('Invalid data');
-    }
-});
-
-// Get all cards
-app.get('/cards', (req, res) => {
-    res.send(cards);
-});
-
-// Delete a card
-app.delete('/cards/:id', (req, res) => {
-    const { id } = req.params;
-    cards = cards.filter(card => card.id !== id);
-    res.send(cards);
-});
-
-// Update a card's initiative
-app.put('/cards/:id', (req, res) => {
-    const { id } = req.params;
-    const { initiative } = req.body;
-    if (!isNaN(initiative)) {
-        const card = cards.find(card => card.id === id);
-        if (card) {
-            card.initiative = initiative;
-            cards.sort((a, b) => b.initiative - a.initiative);
-            res.send(cards);
-        } else {
-            res.status(404).send('Card not found');
-        }
-    } else {
-        res.status(400).send('Invalid initiative');
-    }
 });
 
 app.listen(port, () => {
